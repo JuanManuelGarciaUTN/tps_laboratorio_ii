e140af5e02356f81fed9389e0407af78d4e31cbb
JuanManuelGarciaUTN <artlixUTN@gmail.com>
2022-06-19 12:22:04 -0300

    La Tercera Es La Vencida

--- TP4/ScriptBaseDeDatosOdontologia.sql
+++ TP4/ScriptBaseDeDatosOdontologia.sql
@@ -1,26 +1,146 @@
--->Genero la base de datos y la tabla<--
 USE [master]
 GO
+
+/****** Object:  Database [Odontologia]    Script Date: 19/6/2022 11:57:41 ******/
 CREATE DATABASE [Odontologia]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'Odontologia', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\Odontologia.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'Odontologia_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\Odontologia_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
 GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [Odontologia].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [Odontologia] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [Odontologia] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [Odontologia] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET  ENABLE_BROKER 
+GO
+
+ALTER DATABASE [Odontologia] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [Odontologia] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [Odontologia] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [Odontologia] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [Odontologia] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [Odontologia] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [Odontologia] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [Odontologia] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [Odontologia] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+
+ALTER DATABASE [Odontologia] SET QUERY_STORE = OFF
+GO
+
+ALTER DATABASE [Odontologia] SET  READ_WRITE 
+GO
+
+/****** Object:  Table [dbo].[tablaDeTurnos]    Script Date: 19/6/2022 11:59:12 ******/
 USE [Odontologia]
 GO
+
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE TABLE [dbo].[tablaDeTurnos](
-[fecha] [bigint] NOT NULL PRIMARY KEY,
-[precio] [real] NOT NULL,
-[tipo] [smallint] NOT NULL,
-[telefono] [varchar](20) NOT NULL,
-[nombre] [varchar](50) NOT NULL,
-[apellido] [varchar](50) NOT NULL,
-[dni] [int] NOT NULL,
-[fueAtendido] [bit] NOT NULL
+	[fecha] [bigint] NOT NULL,
+	[precio] [real] NOT NULL,
+	[tipo] [smallint] NOT NULL,
+	[telefono] [varchar](20) NOT NULL,
+	[nombre] [varchar](50) NOT NULL,
+	[apellido] [varchar](50) NOT NULL,
+	[dni] [int] NOT NULL,
+	[fueAtendido] [bit] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[fecha] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
--->Agrego algunos valores para que el historial de atencion no esta vacio<--
+
+-->Añado algunos datos para que no este vacia la tabla al inicio del programa
+-->Esta hecho a mano, asi que todas las consultas tienen el mismo precio, pero es solo un ejemplo
 INSERT [dbo].[tablaDeTurnos] ([fecha], [precio], [tipo], [telefono], [nombre], [apellido], [dni], [fueAtendido]) VALUES (2206180900, 24000, 2, 46461048, 'Okabe', 'Rintaro', 10485960, 1)
 INSERT [dbo].[tablaDeTurnos] ([fecha], [precio], [tipo], [telefono], [nombre], [apellido], [dni], [fueAtendido]) VALUES (2206180930, 24000, 1, 46461048, 'Okabe', 'Rintaro', 10485960, 1)
 INSERT [dbo].[tablaDeTurnos] ([fecha], [precio], [tipo], [telefono], [nombre], [apellido], [dni], [fueAtendido]) VALUES (2206181000, 24000, 0, 46461048, 'Okabe', 'Rintaro', 10485960, 1)
